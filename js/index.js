c03ad6fa4dc294652e74dd1beced2f1663c803a8
Joaquín Fumeaux <joacofum@gmail.com>
2022-07-08 16:27:16 -0300

    mejora 2, se quita location.reload para que no se recargue la página cada vez que se agrega, modifica o elimina un elemento de la tabla

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', inicio, false);
 
-function inicio () {
+function inicio() {
     //Variables del dom van con $ para diferenciarlas
-    const d = document, 
-    $table = d.querySelector(".crud-table"), 
-    $form = d.querySelector(".crud-form"), 
-    $title = d.querySelector(".crud-title"),
-    $template = d.getElementById("crud-template").content,
-    $fragment = d.createDocumentFragment();
+    const d = document,
+        $table = d.querySelector(".crud-table"),
+        $form = d.querySelector(".crud-form"),
+        $title = d.querySelector(".crud-title"),
+        $template = d.getElementById("crud-template").content,
+        $fragment = d.createDocumentFragment();
 
     const getAll = async () => {
         try {
@@ -15,15 +15,7 @@ function inicio () {
                 json = await res.data;
 
             json.forEach(el => {
-                $template.querySelector(".name").textContent = el.nombre;
-                $template.querySelector(".constellation").textContent = el.constelacion;
-                $template.querySelector(".edit").dataset.id = el.id;
-                $template.querySelector(".edit").dataset.name = el.nombre;
-                $template.querySelector(".edit").dataset.constellation = el.constelacion;
-                $template.querySelector(".delete").dataset.id = el.id;
-
-                let $clone = d.importNode($template, true);
-                $fragment.appendChild($clone);
+                crearFilaTabla($template, el, d, $fragment)
             });
 
             $table.querySelector("tbody").appendChild($fragment);
@@ -37,10 +29,10 @@ function inicio () {
     d.addEventListener("submit", async e => {
         if (e.target === $form) {
             e.preventDefault();
-            if (!e.target.id.value ) {
+            if (!e.target.id.value) {
                 //Create - POST
                 let metodo = "POST"
-                ejecutarAjax(metodo, e);             
+                ejecutarAjax(metodo, e);
             } else {
                 //Update - PUT
                 let metodo = "PUT"
@@ -64,19 +56,32 @@ function inicio () {
                 //Delete - DELETE
                 let metodo = "DELETE"
                 ejecutarAjax(metodo, e);
-            }  
+            }
         }
     })
 }
 
-function mostrarError(err){
+function crearFilaTabla($template, el, d, $fragment) {
+    $template.querySelector(".name").textContent = el.nombre;
+    $template.querySelector(".constellation").textContent = el.constelacion;
+    $template.querySelector(".edit").dataset.id = el.id;
+    $template.querySelector(".edit").dataset.name = el.nombre;
+    $template.querySelector(".edit").dataset.constellation = el.constelacion;
+    $template.querySelector(".delete").dataset.id = el.id;
+    $template.querySelector(".fila").id = el.id
+
+    let $clone = d.importNode($template, true);
+    $fragment.appendChild($clone);
+}
+
+function mostrarError(err) {
     let message = err.statusText || "Ocurrió un error";
     alert(`Error ${err.status}: ${message}`);
 }
 
-async function ejecutarAjax(metodo, evento){
+async function ejecutarAjax(metodo, evento) {
     try {
-        if(metodo === "DELETE"){
+        if (metodo === "DELETE") {
             let options = {
                 method: metodo,
                 headers: {
@@ -84,7 +89,7 @@ async function ejecutarAjax(metodo, evento){
                 }
             }
             var res = await axios(`http://localhost:3000/santos/${evento.target.dataset.id}`, options);
-        }else{
+        } else {
             let options = {
                 method: metodo,
                 headers: {
@@ -95,17 +100,54 @@ async function ejecutarAjax(metodo, evento){
                     constelacion: evento.target.constelacion.value
                 })
             }
-            if(metodo === "PUT"){
+            if (metodo === "PUT") {
                 let id = document.getElementById("id");
                 var res = await axios(`http://localhost:3000/santos/${id.value}`, options);
-                id.value = "";
-            }else{
-                var res = await axios("http://localhost:3000/santos", options);     
+
+                $title = document.querySelector(".crud-title")
+                $title.textContent = "Agregar Santo"
+                id.value = ""
+
+
+            } else {
+                var res = await axios("http://localhost:3000/santos", options);
             }
         }
         let json = await res.data;
-        location.reload();
+
+        if (metodo === "POST") {
+            //Agregar el elemento a la tabla sin recargar la página.
+            $table = document.querySelector(".crud-table")
+            $template = document.getElementById("crud-template").content
+            $fragment = document.createDocumentFragment()
+            crearFilaTabla($template, json, document, $fragment)
+            $table.querySelector("tbody").appendChild($fragment)
+        }
+
+        if (metodo === "PUT") {
+            //Actualizar el elemento de la tabla sin recargar la página.
+            let fila = document.getElementById(json.id);
+
+            //Edito nombre y constelación de la fila.
+            fila.children[0].textContent = json.nombre
+            fila.children[1].textContent = json.constelacion
+
+            //Obtengo el botón editar en la fila.
+            let btn_editar = fila.children[2].children[0]
+
+            //Edito los data-name y data-constellation de los botón editar.
+            btn_editar.dataset.name = json.nombre
+            btn_editar.dataset.constellation = json.constelacion
+
+        }
+
+        if (metodo === "DELETE") {
+            //Eliminar elemento de una tabla sin regargar la página.          
+            let id = evento.target.dataset.id;
+            document.getElementById(id).remove();
+        }
+
     } catch (err) {
-        mostrarError(err); 
+        mostrarError(err);
     }
 }
